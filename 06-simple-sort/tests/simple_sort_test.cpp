29788c3a7a4b772c8f7e29d60edfce711ad9582f
Dmitriy Kaznacheev <Dmitriy.Kaznacheev@gmail.com>
2026-02-22 16:30:12 +0500

    [+] hw06: add tests

--- 06-simple-sort/tests/simple_sort_test.cpp
+++ 06-simple-sort/tests/simple_sort_test.cpp
@@ -5,8 +5,50 @@
 #include <simple_sort/insertion_sort.h>
 #include <simple_sort/shell_sort.h>
 
+template <typename T> using It = std::vector<T>::iterator;
+
+template <typename Callable>
+void test(Callable sort, std::string_view test_name,
+          std::string_view test_set) {
+  std::cout << "--- "sv << test_name << " ["sv << test_set << "] ---"sv
+            << std::endl;
+
+  for (int i = 0;; ++i) {
+    auto [ipath, opath] = get_file_paths(std::string(test_set), i);
+
+    auto in_vec = read_vector<int>(std::move(ipath));
+    auto out_vec = read_vector<int>(std::move(opath), in_vec.size());
+    if (in_vec.empty() || out_vec.empty()) {
+      break;
+    }
+
+    {
+      Timer t;
+      sort(std::begin(in_vec), std::end(in_vec));
+      show_result(std::move(in_vec), std::move(out_vec), i, t.duration_ns());
+    }
+  }
+
+  std::cout << std::endl;
+}
+
 int main() {
   using namespace sort;
+
+  // test(bubble::sort<It<int>>, "bubble"sv, "random"sv);
+  // test(bubble::sort<It<int>>, "bubble"sv, "digits"sv);
+  // test(bubble::sort<It<int>>, "bubble"sv, "sorted"sv);
+  // test(bubble::sort<It<int>>, "bubble"sv, "revers"sv);
+
+  // test(insertion::sort<It<int>>, "insertion"sv, "random"sv);
+  // test(insertion::sort<It<int>>, "insertion"sv, "digits"sv);
+  // test(insertion::sort<It<int>>, "insertion"sv, "sorted"sv);
+  // test(insertion::sort<It<int>>, "insertion"sv, "revers"sv);
+
+  // test(shell::sort<It<int>>, "shell"sv, "random"sv);
+  // test(shell::sort<It<int>>, "shell"sv, "digits"sv);
+  // test(shell::sort<It<int>>, "shell"sv, "sorted"sv);
+  // test(shell::sort<It<int>>, "shell"sv, "revers"sv);
 
   return 0;
 }
--- 06-simple-sort/tests/utils.h
+++ 06-simple-sort/tests/utils.h
@@ -3,9 +3,10 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <iterator>
 #include <optional>
 #include <sstream>
-#include <tuple>
+#include <vector>
 
 using namespace std::string_literals;
 using namespace std::string_view_literals;
@@ -19,6 +20,35 @@ inline FilePaths get_file_paths(std::string test_dir, int index) {
   return {ss.str() + ".in"s, ss.str() + ".out"s};
 }
 
+template <typename T>
+std::vector<T> read_vector(std::string file_path, size_t size = 0) {
+  std::fstream f{file_path};
+  if (!f.is_open()) {
+    return {};
+  }
+
+  if (size == 0) {
+    f >> size;
+  }
+
+  std::vector<T> v{std::istream_iterator<T>(f), std::istream_iterator<T>()};
+
+  if (size != v.size()) {
+    v.clear();
+  }
+
+  return v;
+}
+
+template <typename T>
+inline void show_result(std::vector<T> actual, std::vector<T> expected,
+                        int test_index, size_t duration) {
+  bool is_equal{actual == expected};
+  std::cout << (is_equal ? "[ + ]"sv : "[ - ]"sv) << " test_"sv << test_index
+            << ": "sv << std::setw(8) << expected.size() << " "sv << duration
+            << " us"sv << std::endl;
+}
+
 //--- timer -----------------------------------------------
 
 class Timer {
@@ -26,8 +56,8 @@ public:
   Timer() : start_{std::chrono::high_resolution_clock::now()} {}
   size_t duration_ns() {
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_);
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        end_time - start_);
     return duration.count();
   }
 
