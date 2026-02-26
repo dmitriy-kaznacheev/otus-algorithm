917c97608929763847b95c59b2adba5d5bb20e20
Dmitriy Kaznacheev <Dmitriy.Kaznacheev@gmail.com>
2026-02-26 15:50:06 +0500

    [+] hw07: implement insertion sort

--- 07-heap-sort/src/heap_sort/include/heap_sort/selection_sort.h
+++ 07-heap-sort/src/heap_sort/include/heap_sort/selection_sort.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <algorithm>
 
 namespace sort {
 namespace selection {
 
-template <typename It> void sort(It first, It last) {}
+template <typename It> void sort(It first, It last) {
+  for (It it = first; it != last; ++it) {
+    std::iter_swap(it, std::min_element(it, last));
+  }
+}
 
 } // namespace selection
 } // namespace sort
