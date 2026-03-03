d65c5188d0f8885989f2dfbb2a3ac5205c596443
Dmitriy Kaznacheev <Dmitriy.Kaznacheev@gmail.com>
2026-03-03 17:26:13 +0500

    [+] hw08: implement quick sort

--- 08-quick-sort/src/quick_sort/include/quick_sort/quick_sort.h
+++ 08-quick-sort/src/quick_sort/include/quick_sort/quick_sort.h
@@ -1,9 +1,33 @@
 #pragma once
+#include <algorithm>
+#include <iterator>
 
 namespace sort {
 namespace quick {
 
-template <typename It> void sort(It first, It last) {}
+namespace {
+template <typename It> struct Bound {
+  It lhs, rhs;
+};
+
+template <typename It> Bound<It> partition(It first, It last) {
+  auto pivot = *first; // TODO выбрать случайный элемент
+  // clang-format off
+  return {
+    std::partition(first, last, [pivot](const auto &elem) { return (elem <  pivot); }),
+    std::partition(first, last, [pivot](const auto &elem) { return (elem <= pivot); })
+  };
+  // clang-format on
+}
+} // namespace
+
+template <typename It> void sort(It first, It last) {
+  if (first != last) {
+    auto bound = partition(first, last);
+    quick::sort(first, bound.lhs);
+    quick::sort(bound.rhs, last);
+  }
+}
 
 } // namespace quick
 } // namespace sort
