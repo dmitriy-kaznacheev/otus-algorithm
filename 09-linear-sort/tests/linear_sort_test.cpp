297b971025ebe9285c6cc9348b717044caf0512d
Dmitriy Kaznacheev <Dmitriy.Kaznacheev@gmail.com>
2026-03-06 17:37:24 +0500

    [+] hw09: implement helper utils for tests

--- 09-linear-sort/tests/linear_sort_test.cpp
+++ 09-linear-sort/tests/linear_sort_test.cpp
@@ -1,9 +1,12 @@
+#include "utils.h"
+
 #include <linear_sort/bucket_sort.h>
 #include <linear_sort/counting_sort.h>
 #include <linear_sort/radix_sort.h>
 
 int main() {
   using namespace sort;
+
 
   return 0;
 }
