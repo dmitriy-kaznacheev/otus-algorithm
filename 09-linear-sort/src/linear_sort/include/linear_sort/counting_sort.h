fb8f6db91fddbfaec85cea7a952762c109f80b62
Dmitriy Kaznacheev <Dmitriy.Kaznacheev@gmail.com>
2026-03-08 21:27:53 +0500

    [*] hw09: refactoring

--- 09-linear-sort/src/linear_sort/include/linear_sort/counting_sort.h
+++ 09-linear-sort/src/linear_sort/include/linear_sort/counting_sort.h
@@ -10,54 +10,70 @@ namespace counting {
 
 namespace detail {
 
-template <typename T> class Counter final {
-  using Data = std::vector<T>;
-  using Iterator = Data::iterator;
-
-public:
-  template <typename It> Counter(It first, It last) {
-    if (first == last) {
-      return;
-    }
-
-    const auto size = std::distance(first, last);
-    const auto [min_it, max_it] = std::minmax_element(first, last);
-    const auto range = (*max_it) - (*min_it) + 1;
-
-    std::vector<size_t> count(range, 0);
-    for (auto it = first; it != last; ++it) {
-      auto icount = (*it) - (*min_it);
-      ++count[icount];
-    }
-    std::inclusive_scan(count.begin(), count.end(), count.begin());
-
-    data_.resize(size);
-    std::reverse_iterator<It> rfirst{last};
-    std::reverse_iterator<It> rlast{first};
-    for (auto it = rfirst; it != rlast; ++it) {
-      auto icount = (*it) - (*min_it);
-      auto idata = count[icount] - 1;
-      data_[idata] = *it;
-      --count[icount];
-    }
+template <typename Indexer, typename It,
+          typename T = std::iterator_traits<It>::value_type>
+std::vector<size_t> get_count(It first, It last, Indexer indexer, T range) {
+  std::vector<size_t> count(range, 0);
+  for (auto it = first; it != last; ++it) {
+    auto icount = indexer(*it);
+    ++count[icount];
   }
 
-  Iterator begin() { return data_.begin(); }
-  Iterator end() { return data_.end(); }
+  std::inclusive_scan(count.begin(), count.end(), count.begin());
+  return count;
+}
 
-private:
-  Data data_;
-};
+template <typename Indexer, typename It>
+void update_data(It first, It last, Indexer indexer,
+                 std::vector<size_t> &count) {
+  using value_type = std::iterator_traits<It>::value_type;
+  const auto size = std::distance(first, last);
 
-template <typename It>
-Counter(It, It) -> Counter<typename std::iterator_traits<It>::value_type>;
+  std::vector<value_type> data(size, 0);
+
+  std::reverse_iterator<It> rfirst{last};
+  std::reverse_iterator<It> rlast{first};
+  for (auto it = rfirst; it != rlast; ++it) {
+    auto icount = indexer(*it);
+    auto idata = count[icount] - 1;
+    data[idata] = *it;
+    --count[icount];
+  }
+
+  std::move(data.begin(), data.end(), first);
+}
 
 } // namespace detail
 
 template <typename It> void sort(It first, It last) {
+  if (first == last) {
+    return;
+  }
+
   using namespace detail;
-  Counter counter{first, last};
-  std::move(counter.begin(), counter.end(), first);
+  const auto [min_it, max_it] = std::minmax_element(first, last);
+  const auto range = (*max_it) - (*min_it) + 1;
+
+  auto indexer = [&min_it](const auto &value) { return value - (*min_it); };
+
+  auto count = get_count(first, last, indexer, range);
+  update_data(first, last, indexer, count);
+}
+
+template <typename It> void sort_radix(It first, It last, int exp) {
+  if (first == last) {
+    return;
+  }
+
+  using namespace detail;
+  const auto range = 10;
+
+  auto indexer = [exp, range](const auto &value) {
+    return (value / exp) % range;
+  };
+
+  auto count = get_count(first, last, indexer, range);
+  update_data(first, last, indexer, count);
 }
 
 } // namespace counting
--- 09-linear-sort/src/linear_sort/include/linear_sort/radix_sort.h
+++ 09-linear-sort/src/linear_sort/include/linear_sort/radix_sort.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <linear_sort/counting_sort.h>
+
 #include <algorithm>
 #include <iterator>
 #include <numeric>
@@ -8,69 +10,15 @@
 namespace sort {
 namespace radix {
 
-namespace detail {
-
-template <typename T> class Radix final {
-  using Data = std::vector<T>;
-  using Iterator = Data::iterator;
-
-public:
-  template <typename It> Radix(It first, It last) {
-    if (first == last) {
-      return;
-    }
-
-    const auto max = *std::max_element(first, last);
-    const auto size = std::distance(first, last);
-
-    data_.resize(size);
-    std::copy(first, last, data_.begin());
-
-    for (int exp = 1; (max / exp) > 0; exp *= 10) {
-      counting_sort_radix(first, last, exp);
-    }
-  }
-
-  Iterator begin() { return data_.begin(); }
-  Iterator end() { return data_.end(); }
-
-private:
-  template <typename It> void counting_sort_radix(It first, It last, int exp) {
-    const auto range = 10;
-    std::vector<size_t> count(range, 0);
-
-    for (auto it = first; it != last; ++it) {
-      int icount = ((*it) / exp) % range;
-      ++count[icount];
-    }
-
-    std::inclusive_scan(count.begin(), count.end(), count.begin());
-
-    std::reverse_iterator<It> rfirst{last};
-    std::reverse_iterator<It> rlast{first};
-    for (auto it = rfirst; it != rlast; ++it) {
-      auto icount = ((*it) / exp) % range;
-      auto idata = count[icount] - 1;
-      data_[idata] = *it;
-      --count[icount];
-    }
-
-    std::move(data_.begin(), data_.end(), first);
-  }
-
-private:
-  Data data_;
-};
-
-template <typename It>
-Radix(It, It) -> Radix<typename std::iterator_traits<It>::value_type>;
-
-} // namespace detail
-
 template <typename It> void sort(It first, It last) {
-  using namespace detail;
-  Radix radix{first, last};
-  std::move(radix.begin(), radix.end(), first);
+  if (first == last) {
+    return;
+  }
+
+  const auto max = *std::max_element(first, last);
+  for (int exp = 1; (max / exp) > 0; exp *= 10) {
+    counting::sort_radix(first, last, exp);
+  }
 }
 
 } // namespace radix
