78ca48229321a1fb8f61a62325f1a04c8b003987
Dmitriy Kaznacheev <Dmitriy.Kaznacheev@gmail.com>
2026-04-30 20:47:41 +0500

    [*] hw15: refactor -- remove visited vector in demucron_sort

--- 15-graph-alg-demucron/src/demucron/include/demucron/demucron.h
+++ 15-graph-alg-demucron/src/demucron/include/demucron/demucron.h
@@ -9,7 +9,6 @@ std::vector<std::vector<std::size_t>>
 demucron_sort(const AdjacencyMatrix<T> &matrix) {
   size_t n = matrix.get_size();
   std::vector<int> in_degrees = matrix.get_in_degrees();
-  std::vector<bool> visited(n, false);
 
   std::vector<std::vector<std::size_t>> levels;
 
@@ -19,7 +18,7 @@ demucron_sort(const AdjacencyMatrix<T> &matrix) {
 
     // вершины со степенью входа "0", и не посещенные
     for (size_t vertex = 0; vertex != n; ++vertex) {
-      if ((in_degrees[vertex] == 0) && !visited[vertex]) {
+      if (in_degrees[vertex] == 0) {
         current_level.push_back(vertex);
       }
     }
@@ -31,7 +30,7 @@ demucron_sort(const AdjacencyMatrix<T> &matrix) {
 
     // обновление степеней захода
     for (size_t vertex : current_level) {
-      visited[vertex] = true;
+      in_degrees[vertex] = -1;
       ++processed_count;
 
       for (size_t neighbor = 0; neighbor < n; ++neighbor) {
